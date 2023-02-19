f934e9d68bca00e3f6dd8e71231336441aefb753
Vishal Manjhi <vmanjhi98@gmail.com>
2023-02-20 05:20:43 +0530

    second commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,36 +1,5 @@
-import React,{useState} from "react";
+import React from 'react';
 
-const AddTodo = ({ dispatch }) => {
-
-    const [taskArr,setTaskArr] =useState([]);
-    const [inputVal,setVal] = useState("");
-    const [DateId,setId] = useState( new Date().getTime().toString());
-
-    function submitHandler (e) {
-        e.preventDefault();  
-        setId( new Date().getTime().toString()) ;
-        setTaskArr(
-            [...taskArr,
-                {id:DateId,title:inputVal}
-            ]
-        )
-        dispatch({type:"ADD",payload:{title:inputVal,id:DateId}});
-        setVal("");
-    }
-
-
-    function inputFun(e) {
-        setVal(e.target.value);
-    }
-
-    return (
-        <>
-        <form id="todo-form" onSubmit={submitHandler}>
-            <input id="todo-input" type="textarea" required onChange={inputFun} value={inputVal}/>
-            <button type="submit">submit</button>
-        </form>
-        </>
-    )
-}
-
-export { AddTodo }
+ export const Todo  = () =>{
+    
+ }
