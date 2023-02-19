82e445d0cce8b64b306ad8fa976bf315ae5fe85f
Vishal Manjhi <vmanjhi98@gmail.com>
2023-02-20 05:16:13 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { todoReducer } from '../reducers/todo';
 import '../styles/App.css';
-import { Loader } from './Loader';
+import { AddTodo } from './AddTodo';
 import { Todo } from './Todo';
 const App = () => {
+  const [state,dispatch] = useReducer(todoReducer,[])
   
-  
+ 
+  return (
+    <div id="main">
+      <AddTodo dispatch={dispatch} state={state} />
+      <Todo dispatch={dispatch} state={state}/>
+     {/* Render list of Todo Components here */}
+    </div>
+  )
 }
+
 
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,36 @@
- import React from 'react';
+import React,{useState} from "react";
 
- export const Todo  = () =>{
-    
- }
+const AddTodo = ({ dispatch }) => {
+
+    const [taskArr,setTaskArr] =useState([]);
+    const [inputVal,setVal] = useState("");
+    const [DateId,setId] = useState( new Date().getTime().toString());
+
+    function submitHandler (e) {
+        e.preventDefault();  
+        setId( new Date().getTime().toString()) ;
+        setTaskArr(
+            [...taskArr,
+                {id:DateId,title:inputVal}
+            ]
+        )
+        dispatch({type:"ADD",payload:{title:inputVal,id:DateId}});
+        setVal("");
+    }
+
+
+    function inputFun(e) {
+        setVal(e.target.value);
+    }
+
+    return (
+        <>
+        <form id="todo-form" onSubmit={submitHandler}>
+            <input id="todo-input" type="textarea" required onChange={inputFun} value={inputVal}/>
+            <button type="submit">submit</button>
+        </form>
+        </>
+    )
+}
+
+export { AddTodo }
